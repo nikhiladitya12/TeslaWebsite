b33d2ce1181edd2a3906f79997697c5865aded39
Nikhil Aditya <nikhiladitya.22@gmail.com>
2023-06-05 10:24:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,90 @@
+
+var tl = gsap.timeline();
+tl.from("#page1 h1", {
+    rotate:360,
+    scale: 0,
+    duration: 1,
+    delay:1,
+},)
+
+.from("#page2 #topbottomcenter",{
+
+    delay:8,
+    scale:0,
+    duration:1,
+opacity:1,
+   
+    
+    
+},'-=1.4')
+
+.from("#page3 #topbottomcenter",{
+
+    delay:2,
+    scale:0,
+    duration:1,
+opacity:1,
+
+    
+},'-=1.4')
+
+.from("#page4 #topbottomcenter",{
+
+    delay:2,
+    scale:0,
+    duration:1,
+opacity:1,
+
+    
+},'-=1.5')
+
+.from("#page5 #topbottomcenter",{
+
+    delay:2,
+    scale:0,
+    duration:1,
+opacity:1,
+
+    
+},'-=1.5')
+
+.from("#page6 #topbottomcenter",{
+
+    delay:2,
+    scale:0,
+    duration:1,
+opacity:1,
+
+    
+    
+},'-=1.5')
+
+.from("#page7 #topbottomcenter",{
+
+    delay:1.5,
+    scale:0,
+    duration:1,
+opacity:1,
+    
+
+    
+},'-=1.5')
+
+.from("#page8 #bottomcenter",{
+
+    delay:1.5,
+    scale:0,
+    duration:1,
+opacity:1,
+
+    
+},'-=1.5')
+
+.from("#page8 #bottominfo",{
+
+    delay:1.5,
+    scale:0,
+    duration:1,
+opacity:1,
+
+},'-=1.5')
